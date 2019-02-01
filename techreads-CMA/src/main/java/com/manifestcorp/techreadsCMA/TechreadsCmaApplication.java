1ab4b79d4e166d5ed0dcd24dad04585e6d26e3d2
unknown <colton.adkins56@gmail.com>
2019-02-01 16:24:15 -0500

    Hello application

---
+++ techreads-CMA/src/main/java/com/manifestcorp/techreadsCMA/TechreadsCmaApplication.java
@@ -0,0 +1,14 @@
+package com.manifestcorp.techreadsCMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechreadsCmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechreadsCmaApplication.class, args);
+	}
+
+}
+
---
+++ techreads-CMA/src/test/java/com/manifestcorp/techreadsCMA/TechreadsCmaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.manifestcorp.techreadsCMA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechreadsCmaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
